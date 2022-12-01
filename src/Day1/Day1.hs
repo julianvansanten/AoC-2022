8ae94600a25534aa2fa8c8869c6f6a97f8f3eacb
julianvansanten <julianvs0501@gmail.com>
2022-12-01 20:45:31 +0100

    Solved day1.

--- src/Day1/Day1.hs
+++ src/Day1/Day1.hs
@@ -1,12 +1,26 @@
 module Day1.Day1 (getDaySolutions) where
 
 
+import Data.List (sort, reverse)
+import Data.List.Split (splitOn)
+
+
 getDaySolutions :: (String -> String, String -> String)
 getDaySolutions = (solve1, solve2)
 
 
 solve1 :: String -> String
-solve1 = error "First solution of day 1 not implemented yet!"
+solve1 str = show (maximum $ getWeights str)
 
 solve2 :: String -> String
-solve2 = error "Second solution of day 1 not implemented yet!"
+solve2 = show . getTopThree
+
+
+-- | Get the weights of all Calories carried by each elf.
+getWeights :: String -> [Int]
+getWeights str = map (sum . (map read . lines)) $ splitOn "\n\n" str
+
+
+-- | Get the total weight of the top three elves
+getTopThree :: String -> Int
+getTopThree = sum . take 3 . reverse . sort . getWeights
