57a4327d9c8824a8b30f74a6f5273afd26769377
julianvansanten <julianvs0501@gmail.com>
2022-12-02 10:21:02 +0100

    Added parsec as dependency. Added parsers for draws.

--- src/Day2/Parser.hs
+++ src/Day2/Parser.hs
@@ -10,4 +10,32 @@ spaces :: Parser ()
 spaces = skipMany1 space
 
 
--- TODO Implement a parser for a RPS game.
+-- TODO test if implementation correct.
+-- TODO implement game parser.
+
+parseOpponentDraw :: Parser Draw
+parseOpponentDraw = parseORock <|> parseOPaper <|> parseOScissors
+
+
+parseMeDraw :: Parser Draw
+parseMeDraw = parseMRock <|> parseMPaper <|> parseMScissors
+
+
+parseORock :: Parser Draw
+parseORock = char 'A' >> return Rock
+
+parseOPaper :: Parser Draw
+parseOPaper = char 'B' >> return Paper
+
+parseOScissors :: Parser Draw
+parseOScissors = char 'C' >> return Scissors
+
+
+parseMRock :: Parser Draw
+parseMRock = char 'X' >> return Rock
+
+parseMPaper :: Parser Draw
+parseMPaper = char 'Y' >> return Paper
+
+parseMScissors :: Parser Draw
+parseMScissors = char 'Z' >> return Scissors
