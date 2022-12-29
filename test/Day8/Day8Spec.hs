dd5103f3170e535bacec4c343e84352092411049
julianvansanten <julianvs0501@gmail.com>
2022-12-29 16:13:46 +0100

    Add solve1 verification test for given example.

--- test/Day8/Day8Spec.hs
+++ test/Day8/Day8Spec.hs
@@ -1,19 +1,28 @@
 module Day8.Day8Spec where
 
 
-import Test.Hspec ( Spec, describe, it )
+import Test.Hspec ( Spec, describe, it, shouldBe )
 
+import Day8.Day8 ( getDaySolutions )
 
 spec :: Spec
 spec = do
     test1
     test2
 
-
 test1 :: Spec
-test1 = describe "solve1" $ do
-    it "no test for day 8 implemented yet!" True
+test1 = describe "Day8.Day8.solve1" $ do
+    it "should give the correct number of visible trees" $ do
+        fst getDaySolutions input `shouldBe` "21"
 
 test2 :: Spec
 test2 = describe "solve2" $ do
     it "no test for day 8 implemented yet!" True
+
+
+input :: String
+input = "30373\n" ++
+    "25512\n" ++
+    "65332\n" ++
+    "33549\n" ++
+    "35390"
